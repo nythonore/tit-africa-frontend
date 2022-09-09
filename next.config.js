8c22c01ab0da3ee92429c4643bc243608b4dc9ed
nythonore <niyitegekah@gmail.com>
2022-09-09 18:45:36 +0200

    fix bug

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	reactStrictMode: true,
+	swcMinify: true,
 
-module.exports = nextConfig
+	experimental: {
+		images: {
+			unoptimized: true,
+		},
+	},
+};
+
+module.exports = nextConfig;
